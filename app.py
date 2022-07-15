9a7ac73bf27c92d374bb7522352554c9b4b58ce6
HWaiChan <hanswai93@gmail.com>
2022-07-15 20:17:15 +0100

    dvc integration

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, request, url_for, redirect, render_template, jsonify
 import anime
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system(f"dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 # Initalise the Flask app
 app = Flask(__name__)
