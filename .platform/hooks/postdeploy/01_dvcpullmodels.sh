64bdbc793d7fb350da95c0ef83a6a5614dae1cb5
HWaiChan <hanswai93@gmail.com>
2022-07-29 22:36:44 +0100

    fetch model at runtime

--- .platform/hooks/postdeploy/01_dvcpullmodels.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-pip install dvc[gdrive]
-mkdir -p .dvc/tmp
-dvc remote modify myremote gdrive_use_service_account true
-echo "${GDRIVE_CREDENTIALS_DATA}" > .dvc/tmp/credentials.json
-dvc remote modify --local myremote gdrive_service_account_json_file_path .dvc/tmp/credentials.json
-dvc pull -v
--- app.py
+++ app.py
@@ -29,6 +29,30 @@ def similar_items():
 #         exit("dvc pull failed")
 #     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
+def load_up_models():
+    if os.path.isdir(".dvc"):
+        os.system("dvc config core.no_scm true")
+        Path(".dvc/tmp").mkdir(parents=True, exist_ok=True)
+        gdrive_data = os.getenv("GDRIVE_CREDENTIALS_DATA")
+        f = open(".dvc/tmp/credentials.json", "w")
+        f.write(gdrive_data)
+        f.close()
+        os.system(
+            "dvc remote modify --local myremote gdrive_service_account_json_file_path .dvc/tmp/credentials.json"
+        )
+        if os.system(f"dvc pull") != 0:
+            exit("dvc pull failed")
+        if os.path.exists("model_lite_KNNBasic.pickle"):
+            print("model_lite_KNNBasic.pickle exists")
+            try:
+                os.system("rm -r .dvc .apt/usr/lib/dvc")
+            except OSError as error:
+                print("OS Error: {0}".format(error))
+        else:
+            print("model_lite_KNNBasic.pickle doesn't exist")
+
 if __name__ == "__main__":
+    load_up_models()
     app.run()
+
     print("App is running....")
